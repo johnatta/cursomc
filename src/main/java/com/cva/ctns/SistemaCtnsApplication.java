2f46ab50af206f072c0890f7f1e45e62a78204ed
Johnatta costa <johnatta.costa@outlook.com>
2021-01-27 00:30:27 -0300

    Projeto foi criado

---
+++ src/main/java/com/cva/ctns/SistemaCtnsApplication.java
@@ -0,0 +1,13 @@
+package com.cva.ctns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCtnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCtnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cva/ctns/SistemaCtnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cva.ctns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCtnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
